41441977005f9cd78f44a781f0d9344524c24ad3
Austin Gabel <agabel@gmail.com>
2013-10-23 11:45:52 -0500

    Adjusted the setup file

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+#!/usr/bin/python
 
-setup(name='transcoder',
-      version='0.1',
-      description='Integration library for the Nex-Tech transcoder',
-      author='Austin Gabel',
-      author_email='agabel@gmail.com',
-      url='https://github.com/nex-tech/TranscoderPy',
-      license="MIT License",
-      install_requires=['requests>=1.0'],
-      packages=['transcoder'],
-      platforms='any',
+from distutils.core import setup
+from transcoder.utils import __version__
+
+
+setup(
+    name='transcoder',
+    version=__version__,
+    description='Integration library for the Nex-Tech transcoder',
+    author='Austin Gabel',
+    author_email='agabel@gmail.com',
+    url='https://github.com/nex-tech/TranscoderPy',
+    platforms='any',
+    install_requires=[
+        'requests>=1.0'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    packages=[
+        'transcoder'
+    ],
 )
 
